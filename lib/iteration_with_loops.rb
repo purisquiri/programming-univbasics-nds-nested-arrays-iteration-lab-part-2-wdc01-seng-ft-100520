44b7cf4569ef722587f901a8e96a576b81ca7f14
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-28 13:43:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,9 +3,9 @@ def find_min_in_nested_arrays(src)
   min_week_temperatures = []
   while row_index < src.count do
     element_index = 0
-    min_daily_temperature = 30
+    min_daily_temperature = 0
     while element_index < src[row_index].count do
-      if src[row_index][element_index] == src[row_index][element_index].min min_daily_temperature
+      if src[row_index][element_index] == src[row_index][element_index].min 
         min_daily_temperature = src[row_index][element_index] 
       end
     element_index += 1
